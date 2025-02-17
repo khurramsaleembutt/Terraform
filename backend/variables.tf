0cd85b73df521a6093b62ffecd87cd4f9510b4b0
khurramsaleembutt <khurramsaleembutt@yahoo.com>
2025-02-17 13:11:18 +0500

    Re-add .gitignore to ensure proper file ignoring

--- backend/variables.tf
+++
@@ -1,9 +0,0 @@
-
-#Required: AWS Profile. Either put default in the variable input or leave it blank to keep the terraform execution working
-variable "profile" {
-  default = "default"
-}
-#Required: AWS Region. This one will have priority over aws environment variables or configuration file credentials
-variable "region" {
-  default = "ap-south-1"
-}
